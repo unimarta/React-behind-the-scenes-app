66056c6224f5437f3976a0cb65a0694be2d1f022
unimarta <kramkagitarzystka@yahoo.pl>
2022-03-02 16:33:51 +0100

    Added conditional component toggle updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <p>This is new!</p>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
